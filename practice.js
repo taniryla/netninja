b40bb139d7f5697a552773f7a00e6624fa957dad
taniryla <ryland0000@gmail.com>
2022-01-10 01:53:49 -0700

    partial templates exercises

--- practice.js
+++ practice.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs'); // making EJS our view engine
-
-app.get('/', (req, res) => {
-    res.render("profile"); // index refers to index.ejs
-})
+app.set('view engine', 'ejs'); // mamking EJS our view engine
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -17,7 +13,7 @@ app.get('/contact', (req, res) => {
 
 // we can query into a database using a route parameter
 app.get('/profile/:name', (req, res) => {
-    const data = { age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing'] };
+    const data = { age: 29, job: 'ninja' }
     res.render('profile', { person: req.params.name, data: data });
 })
 
